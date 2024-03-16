9704b5d6e834d1fe7284c97f5326e4180e42bc86
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-03-16 17:00:11 -0400

    commit 2
    update - fleshing out code - trying to connect files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// using event handlers
+
 function redText() {
 
 }
@@ -7,5 +9,23 @@ function blueText() {
 }
 
 function greenText() {
-    
+
+}
+
+// using event Listeners 
+const jsButton = document.getElementById('jsBtn);
+jsButton.addEventListener('mouseover', backgroundToPeach);
+jsButton.addEventListener('mouseout', backgroundToGrey);
+jsButton.addEventListener('click', backgroundToPurple);
+
+function backgroundToPeach() {
+    jsDiv.style.background = "salmon";
+}
+
+function backgroundToGrey() {
+    jsDiv.style.background = "lightgrey";
+}
+
+function backgroundToPurple() {
+    jsDiv.style.background = "purple";
 }
