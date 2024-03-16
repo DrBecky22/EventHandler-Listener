7c1e32bd3638f5e5d50edf7944a0910091ed26df
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-03-16 17:44:38 -0400

    commit
    code complete and working! OMG

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // using event handlers
+const textToChange = document.querySelector('colorText');
 
 function redText() {
-
+    colorText.style.color = 'crimson';
 }
 
 function blueText() {
-
+    colorText.style.color = 'royalblue';
 }
 
 function greenText() {
-
+    colorText.style.color = 'green';
 }
 
+function boldText() {
+    colorText.style.fontWeight = 'bold';
+}
+    
+
+
 // using event Listeners 
-const jsButton = document.getElementById('jsBtn);
+const jsButton = document.getElementById('jsBtn');
+const jsDiv = document.querySelector('#divTwo');
 jsButton.addEventListener('mouseover', backgroundToPeach);
 jsButton.addEventListener('mouseout', backgroundToGrey);
 jsButton.addEventListener('click', backgroundToPurple);
